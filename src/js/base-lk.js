bac55be2ef26a51857c05de4d61698c79c3c6564
Dina Hrist <hdina@inbox.ru>
2019-12-04 23:16:42 +0700

    Added lk-admin-unloading page

--- src/js/base-lk.js
+++ src/js/base-lk.js
@@ -13,6 +13,14 @@ class BaseLk {
         this.radio = `${selector}-radio`;
         this.radioBtn = `${this.radio}-btn`;
 
+        this.tag = `${selector}-tag`;
+        this.tagList = `${this.tag}-list`;
+        this.tagWrap = `${this.tag}-wrap`;
+        this.tagEmpty = `${this.tag}-empty`;
+        this.tagDelete = `${this.tag}-delete`;
+        this.tagSelect = `${this.tag}-select`;
+        this.tagSelectBtn = `${this.tagSelect}-btn`;
+
         this.init();
     }
 
@@ -20,7 +28,63 @@ class BaseLk {
         this.clickRowAdd();
         this.changeRadio();
         this.initField();
+        this.clickOnSelectBtn();
+        this.clickOnTagDelete();
     }
+
+
+    clickOnSelectBtn() {
+        const self = this;
+        $(this.tagSelectBtn).on('click', function () {
+
+            const select = $(this).siblings(`${self.tagSelect}`).find(':selected');
+            const valueSelect = select.val();
+            const textSelect = select.text();
+            if (!valueSelect) return;
+
+            let list = $(self.tagList);
+            const nameTag = list.data('name') || 'tags[]';
+
+            const hasTags = list.find(`${self.tag} input[value=${valueSelect}]`);
+            if (hasTags.length) return;
+
+            let tag = self.getTagTemplate(textSelect, valueSelect, nameTag);
+            list.append(tag);
+
+            if (list.find(self.tag).length) {
+                let wrap = list.parent(self.tagWrap);
+                wrap.addClass('active');
+                wrap.siblings(self.tagEmpty).removeClass('active');
+            }
+        });
+    }
+
+    clickOnTagDelete() {
+        const self = this;
+        $(document).on('click', this.tagDelete, function () {
+            $(this).parent(self.tag).remove();
+
+            let list = $(self.tagList);
+            if (!list.find(self.tag).length) {
+                let wrap = list.parent(self.tagWrap);
+                wrap.removeClass('active');
+                wrap.siblings(self.tagEmpty).addClass('active');
+            }
+        });
+    }
+
+    getTagTemplate(text, value, name) {
+        const classDelete = this.tagDelete.slice(1);
+        const classTag = this.tag.slice(1);
+        return (
+            `<li class="${classTag}">
+                <span>${text}</span>
+                <i class="${classDelete}"></i>
+                <input type="hidden" name="${name}" value="${value}">
+            </li>`
+        );
+    }
+
 
     clickRowAdd() {
         const self = this;
